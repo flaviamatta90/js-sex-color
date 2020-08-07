fbe84d39f2dbf7f4da5617e4236d8ab3b745519a
Flavia <flaviamatta90@gmail.com>
2020-08-07 10:25:52 +0200

    agg prompt + if sesso

--- js/script.js
+++ js/script.js
@@ -5,7 +5,13 @@ console.log('ciao ' + nome);
 var sesso = prompt('Sei femmina o maschio?');
 console.log('il tuo sesso è ' + sesso);
 
-document.getElementById('nome-utente').innerHTML = nome;
+if (sesso != 'maschio' || sesso != 'femmina') {
+  sesso = prompt('Ri inserisci il sesso');
+}
+
+var nomeUtente = document.getElementById('nome-utente');
+
+nomeUtente.innerHTML = nome;
 
 // 3 Sulla pagina deve apparire ciao e nome
 // il colore del nome deve essere rosa se è femmina e azzurro se è maschio
@@ -13,9 +19,9 @@ document.getElementById('nome-utente').innerHTML = nome;
 
 if (sesso == 'maschio')
 {
-  document.getElementById('nome-utente').style.color = '#090c9b';
+  nomeUtente.style.color = '#090c9b';
 } else if (sesso == 'femmina'){
-  document.getElementById('nome-utente').style.color = '#e43f6f';
+  nomeUtente.style.color = '#e43f6f';
 } else{
   alert('Hai sbagliato qualcosa!')
 }
